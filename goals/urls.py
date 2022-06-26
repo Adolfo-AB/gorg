82f0bb3bdb08ff3a6fd8ab8ed590858d5d4c0be4
AdolfoAB <adolfoab6@gmail.com>
2022-06-26 11:57:16 +0200

    Adds buttons to delete and complete

--- goals/urls.py
+++ goals/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
 
     path("goals/new", views.GoalCreateView.as_view(), name="goals.new"),
     path("goals/<int:pk>", views.GoalUpdateView.as_view(), name="goals.update"),
+    path("goals/<int:pk>/complete", views.GoalCompleteView.as_view(), name="goals.complete"),
     path("goals/<int:pk>/delete", views.GoalDeleteView.as_view(), name="goals.delete"),
 
     path('signup', views.SignupView.as_view(), name='signup'),
